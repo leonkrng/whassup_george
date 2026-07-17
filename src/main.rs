ac1649dda1e722bb3a088c5ae82566ac2a2d07ec
leonkrng <kroening.social@gmail.com>
2026-07-17 21:26:23 +0200

    Getting RSS feed

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
+use reqwest::blocking::Response;
 use rss::Channel;
 use std::io::Cursor;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let body = reqwest::blocking::get(
-        "https://georgerrmartin.com/notablog/feed",
-    )?
-    .bytes()?;
+fn main() {
+    let latest_post: Result<rss::Item, Box<dyn std::error::Error>> = get_latest_post();
 
-    let channel = Channel::read_from(Cursor::new(body))?;
+    let latest_post: rss::Item = match latest_post {
+        Ok(post) => post,
+        Err(error) => panic!("{}", error)
+    };
 
-    println!("Feed Title: {}", channel.title());
+    println!("Feed Title: {:?}", latest_post.title);
+}
 
-    Ok(())
+fn get_latest_post() -> Result<rss::Item, Box<dyn std::error::Error>> {
+
+    let body: Response = reqwest::blocking::get("https://georgerrmartin.com/notablog/feed")?;
+    let bytes = body.bytes()?;
+    let channel = Channel::read_from(Cursor::new(bytes))?;
+
+    return Ok(channel.items()[0].clone());
 }
