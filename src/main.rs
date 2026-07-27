12f67da575943fcb8845cf8cec5e8c39ddd1d7ab
leonkrng <kroening.social@gmail.com>
2026-07-27 20:47:12 +0200

    Reading Json files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 use reqwest::blocking::Response;
 use rss::Channel;
-use std::io::Cursor;
+use std::{fs::File, io::{BufReader, Cursor}};
+use serde::{Deserialize, Serialize, de::DeserializeOwned};
+use std::path::Path;
+
+#[derive(Serialize, Deserialize)]
+struct Config {
+    sender_email: String,
+    receiver_email: String
+}
+
+#[derive(Serialize, Deserialize)]
+struct Data {
+    last_blog_title: String,
+}
 
 fn main() {
     let latest_post: Result<rss::Item, Box<dyn std::error::Error>> = get_latest_post();
@@ -11,6 +24,22 @@ fn main() {
     };
 
     println!("Feed Title: {:?}", latest_post.title);
+
+    let config_path = Path::new("./src/config/config.json");
+    let config: Result<Config, Box<dyn std::error::Error>>= get_from_json(config_path);
+    let config: Config = match config  {
+        Ok(config) => config,
+        Err(error) => panic!("{}", error),
+    };
+    println!("{:?}, {:?}", config.sender_email, config.receiver_email);
+
+    let data_path = Path::new("./src/config/data.json");
+    let data: Result<Data, Box<dyn std::error::Error>> = get_from_json(data_path);
+    let data: Data = match data {
+        Ok(data) => data,
+        Err(error) => panic!("{}", error),
+    };
+    println!("{:?}", data.last_blog_title);
 }
 
 fn get_latest_post() -> Result<rss::Item, Box<dyn std::error::Error>> {
@@ -20,4 +49,13 @@ fn get_latest_post() -> Result<rss::Item, Box<dyn std::error::Error>> {
     let channel = Channel::read_from(Cursor::new(bytes))?;
 
     return Ok(channel.items()[0].clone());
+}
+
+fn get_from_json<T: DeserializeOwned>(path: &Path) -> Result<T, Box<dyn std::error::Error>> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+
+    let u = serde_json::from_reader(reader)?;
+    return Ok(u);
 }
